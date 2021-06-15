5f1bcddc012fc57aa798f9359685ebdf61600dc8
Michael R. Crusoe <michael.crusoe@gmail.com>
2021-06-15 15:32:05 +0200

    enable running tests from the root of the project

--- wdl2cwl/tests/test_cwl.py
+++ wdl2cwl/tests/test_cwl.py
@@ -1,20 +1,22 @@
 import pytest
 import filecmp
 
+import os.path
 import sys
-sys.path.append("..")
 
 from .. import WdlV1_1 as wdl
 
 from subprocess import call
 
+def get_file(path: str) -> str:
+    return os.path.join(os.path.dirname(__file__), path)
 
 class Test:
 
     def testCollectQualityYieldMetrics(self):
-        abc = wdl.main(["wdl_files/Qc.wdl"])
+        abc = wdl.main([get_file("wdl_files/Qc.wdl")])
         testStr = ""
-        with open('cwl_files/CollectQualityYieldMetrics.cwl','r') as file:
+        with open(get_file('cwl_files/CollectQualityYieldMetrics.cwl'),'r') as file:
             testStr = file.read()
 
         assert abc == testStr
