2c571bd8c30a0938a643158a488ce84238592158
Michael R. Crusoe <michael.crusoe@gmail.com>
2021-06-16 17:14:40 +0200

    fix docstrings

--- wdl2cwl/tests/test_cwl.py
+++ wdl2cwl/tests/test_cwl.py
@@ -8,11 +8,11 @@ def get_file(path: str) -> str:
     return os.path.join(os.path.dirname(__file__), path)
 
 
-class Test:
-    def testCollectQualityYieldMetrics(self) -> None:
-        abc = wdl.main([get_file("wdl_files/Qc.wdl")])
-        testStr = ""
-        with open(get_file("cwl_files/CollectQualityYieldMetrics.cwl"), "r") as file:
-            testStr = file.read()
+def testCollectQualityYieldMetrics() -> None:
+    """Test a single WDL to CWL conversion."""
+    abc = wdl.main([get_file("wdl_files/Qc.wdl")])
+    testStr = ""
+    with open(get_file("cwl_files/CollectQualityYieldMetrics.cwl"), "r") as file:
+        testStr = file.read()
 
-        assert abc == testStr
+    assert abc == testStr
