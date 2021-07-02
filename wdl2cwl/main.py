cad11b53fea34e8658a833f07fee66b15cbfd225
dinithiW <diniwick124@gmail.com>
2021-07-02 16:58:04 +0530

    add command line option to output result

--- wdl2cwl/main.py
+++ wdl2cwl/main.py
@@ -1,4 +1,6 @@
 """Main entrypoint for WDL2CWL."""
+import argparse
+from argparse import Namespace
 import sys
 from io import StringIO
 from typing import List, cast
@@ -91,9 +93,9 @@ def get_command(
     return new_command
 
 
-def main(argv: List[str]) -> str:
+def convert(args: Namespace) -> str:
     """Generate a CWL object to match "cat-tool.cwl"."""
-    f = open(argv[0])
+    f = open(args.workflow)
     text = InputStream(f.read())
     lexer = WdlV1_1Lexer(text)
     stream = CommonTokenStream(lexer)
@@ -129,7 +131,6 @@ def main(argv: List[str]) -> str:
     command = get_command(
         command, ast.task_inputs, ast.task_inputs_bound, input_types, input_names
     )
-    print(command)
 
     base_command = ["sh", "example.sh"]
 
@@ -217,8 +218,22 @@ def main(argv: List[str]) -> str:
     # ^ converts multine line strings to nice multiline YAML
     yaml.dump(cwl_result, result_stream)
     yaml.dump(cwl_result, sys.stdout)
+
+    if args.directory:
+        with open(args.directory, "w") as result:
+            result.write(str(result_stream.getvalue()))
+
     return result_stream.getvalue()
 
 
+def main() -> None:
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("workflow", help="WDL workflow")
+    parser.add_argument("-d", "--directory", help="Directory to store CWL files")
+    args = parser.parse_args()
+    convert(args)
+
+
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
