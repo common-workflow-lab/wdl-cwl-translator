ca0c5518bc599606ac6dd54a8d4d6c807b9bdfa7
dinithiW <diniwick124@gmail.com>
2021-07-06 17:03:59 +0530

    add argparse test as a separate test.

--- wdl2cwl/tests/test_cwl.py
+++ wdl2cwl/tests/test_cwl.py
@@ -8,6 +8,17 @@ def get_file(path: str) -> str:
     return os.path.join(os.path.dirname(__file__), path)
 
 
+@pytest.fixture(autouse=True)
+def mock_args(monkeypatch: pytest.MonkeyPatch, wdl_path: str) -> None:
+    monkeypatch.setattr(
+        "sys.argv",
+        [
+            "python",
+            get_file(wdl_path),
+        ],
+    )
+
+
 @pytest.mark.parametrize(
     "wdl_path,cwl_path",
     [
@@ -18,15 +29,8 @@ def get_file(path: str) -> str:
         ),
     ],
 )
-def test_wdls(monkeypatch: pytest.MonkeyPatch, wdl_path: str, cwl_path: str) -> None:
+def test_wdls(wdl_path: str, cwl_path: str) -> None:
     """Test WDL to CWL conversion."""
-    monkeypatch.setattr(
-        "sys.argv",
-        [
-            "python",
-            get_file(wdl_path),
-        ],
-    )
     convertedStr = wdl.main()
     testStr = ""
     with open(get_file(cwl_path)) as file:
