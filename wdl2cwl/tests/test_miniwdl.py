f57913a6d96540885966f61915c843c301142e4e
Dennis Chukwunta <chuksmcdennis@yahoo.com>
2021-12-15 03:49:24 -0500

    Miniwdl manual creation (#107)
    * first draft with miniwdl

---
+++ wdl2cwl/tests/test_miniwdl.py
@@ -0,0 +1,31 @@
+"""Tests for miniwdl."""
+import os.path
+import pytest
+import pathlib
+import filecmp
+
+from .. import main_oop as wdl
+
+
+def get_file(path: str) -> str:
+    """Get file."""
+    return os.path.join(os.path.dirname(__file__), path)
+
+
+@pytest.mark.parametrize(
+    "wdl_path,cwl_path",
+    [
+        ("wdl_files/bowtie_1.wdl", "oop_cwl_files/bowtie_1.cwl"),
+    ],
+)
+class TestParameterized:
+    """Contains the test functions for WDL to CWL conversion."""
+
+    def test_wdls(self, wdl_path: str, cwl_path: str) -> None:
+        """Test WDL to CWL conversion."""
+        convertedStr = wdl.convert(get_file(wdl_path))
+        testStr = ""
+        with open(get_file(cwl_path)) as file:
+            testStr = file.read()
+
+        assert convertedStr == testStr
