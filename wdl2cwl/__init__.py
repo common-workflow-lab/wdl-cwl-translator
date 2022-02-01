bd25bb84fbb2e72d8f1bb9f834bc51a526b68020
Michael R. Crusoe <michael.crusoe@gmail.com>
2022-02-01 11:53:54 +0100

    use logging from the stdlib

---
+++ wdl2cwl/__init__.py
@@ -0,0 +1,6 @@
+"""Module to convert (Open)WDL documents to CWL v1.2."""
+import logging
+
+_logger = logging.getLogger("wdl2cwl")
+_logger.addHandler(logging.StreamHandler())
+_logger.setLevel(logging.INFO)
